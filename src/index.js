02e9cfa8593f32c7a2a1d34eb7a1a362a32a150f
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2021-04-05 13:58:26 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { Client, Collection } = require("discord.js");
 const fs = require("fs");
 const client = new Client();
 
+const sleds = new Collection(require("./sleds"));
 const { prefix, token } = require("./config");
 
 client.on("ready", () => {
@@ -14,11 +15,26 @@ client.on("ready", () => {
 });
 
 client.on("message", async (message) => {
-  if(!message.content.startsWith(prefix) || message.author.bot) return;
-  const [cmd, ...args] = message.content.slice(prefix.length).split(" ");
-  if(!client.commands.has(cmd)) return;
-  const handler = await client.commands.get(cmd);
-  await handler({ message, args, client });
+  if(message.content.startsWith(prefix)) {
+    const [cmd, ...args] = message.content.slice(prefix.length).split(" ");
+    if(!client.commands.has(cmd)) return;
+    const handler = await client.commands.get(cmd);
+    await handler({ message, args, client });
+  } else {
+    if(!sleds.has(message.channel.name)) return;
+    client.channels.cache.filter(ch => ch.name === message.channel.name)
+    .forEach(ch => {
+      ch.send(
+        new MessageEmbed()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setDescription(message.content)
+        .setFooter(message.guild.name, message.guild.iconURL())
+      );
+    });
+    message.react(":outbox_tray:")
+    .then(() => {
+      setTimeout(() => (message.reactions.cache.get(":outbox_tray:").users.remove()), 2000);
+    });
 });
 
 client.login(token);
