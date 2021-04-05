b8ca1fa2aafb3e617008b435ab430e9ae394c627
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2021-04-05 13:27:50 +0900

    Update create.js

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,1 +1,18 @@
+const { MessageEmbed } = require("discord.js");
+const fs = require("fs");
+const { prefix }  = require("../config");
+const sleds = new Map(require("../sleds"));
 
+module.exports = {
+  name: "create",
+  fn: ({ message, args }) => {
+    const name = args.join(" ");
+    if(!sleds.has(name)) return message.reply(`${name}というスレッドは既に存在しています。`);
+    sleds.set(name, { user: message.author.id, date: Date.now() });
+    message.author.send(
+      new MessageEmbed()
+      .setTitle("成功")
+      .setDescription(`スレッドを作成しました。\n削除したい場合は作成したチャンネルで\`${prefix}delete\`と発言してください。`)
+    );
+  }
+}
