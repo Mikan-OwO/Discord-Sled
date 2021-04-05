325832dae4cbb4ec8671a6f8f38fcbf4903de9ec
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2021-04-05 13:31:28 +0900

    Update create.js

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require("discord.js");
 const fs = require("fs");
+const path = require("path");
 const { prefix }  = require("../config");
 const sleds = new Map(require("../sleds"));
 
@@ -14,5 +15,8 @@ module.exports = {
       .setTitle("成功")
       .setDescription(`スレッドを作成しました。\n削除したい場合は作成したチャンネルで\`${prefix}delete\`と発言してください。`)
     );
+    fs.writeFile(path.join(__dirname, "../sleds.json"), JSON.stringify([...sleds]), (e) => {
+      if(e) console.error(e);
+    });
   }
 }
